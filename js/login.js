0f337548294000e48720559711821daf9b8ef56d
Isabelle Pak <s10222456@connect.np.edu.sg>
2024-01-25 13:07:13 +0800

    feat: Login to TSAO page

--- js/login.js
+++ js/login.js
@@ -1,41 +1,65 @@
 async function sendAuthRequest() {
-    // Get the values from the text inputs
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Create the JSON object
     const data = {
         Username: username,
         Password: password
     };
 
-    // Set the request options
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
     };
 
     try {
-        // Send the POST request
         const response = await fetch('http://localhost:8000/api/auth', requestOptions);
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+        if (response.status === 202) {
+            document.getElementById("error-message").innerHTML = '';
+            alert('Login successful.');
 
-        // Handle the response
-        const responseData = await response.json();
-        console.log('Response:', responseData);
+            const resData = await response.json();
+            console.log('Response:', resData);
+            setCookie('jwtToken',resData["Token"],30)
+            
+            sessionStorage.setItem("ID", resData["ID"])
+            sessionStorage.setItem("Name", resData["Name"])
+            sessionStorage.setItem("Role", resData["Role"])
+            
+            window.location.href = "../index.html"
+        } else if (response.status === 403) {
+            const outputHTML = 'Error: Incorrect username or password! Please try again.';
+            document.getElementById("error-message").innerHTML = outputHTML;
+        }
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
 document.getElementById('loginButton').addEventListener('click', function(event) {
-    // Prevent the default form submission behavior
     event.preventDefault();
-    
-    // Call the sendAuthRequest function
     sendAuthRequest();
 });
