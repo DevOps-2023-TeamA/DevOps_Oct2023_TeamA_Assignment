e7cba1091c6492b5ab3eed9333648ea2fa69bad7
Isabelle Pak <s10222456@connect.np.edu.sg>
2024-02-04 22:05:27 +0800

    feat: edit account information (completed)

--- js/edit-account.js
+++ js/edit-account.js
@@ -1,14 +1,77 @@
 window.onload = () => {
     const nameField = document.getElementById("name").setAttribute("value", localStorage.getItem("tempName"))
     const usernameField = document.getElementById("username").setAttribute("value", localStorage.getItem("tempUsername"))
-    const roleField = document.getElementById("role").innerHTML += `${localStorage.getItem("tempRole")} <i class="mdi mdi-chevron-down"></i>`
+    const roleField = document.getElementById("role").innerHTML += `${localStorage.getItem("tempRole")}<i class="mdi mdi-chevron-down"></i>`
 
     localStorage.removeItem("tempName")
     localStorage.removeItem("tempUsername")
     localStorage.removeItem("tempRole")
 }
 
-document.getElementById('logoutButton').addEventListener('click', function(event) {
+document.getElementById('editAccButton').addEventListener('click', function(event) {
     event.preventDefault();
-    eraseCookie("jwtToken");
+    editAccount();
 });
+
+document.getElementById('cancelButton').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = "../accounts-management.html";
+});
+
+function selectRole(role) {
+    document.getElementById('role').innerHTML = `${role}<i class="mdi mdi-chevron-down"></i>`;
+}
+
+async function editAccount() {
+    // Get name, username and role values
+    const name = document.getElementById("name").value
+    const username = document.getElementById("username").value
+    const role = document.getElementById("role").innerText
+
+    // Define data to be sent
+    const data = {
+        Name: name,
+        Username: username,
+        Role: role,
+    };
+
+    // Define request options
+    const requestOptions = {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+    };
+
+    try {
+        // Send request to server
+        const response = await fetch(`http://localhost:8002/api/accounts/${localStorage.getItem("tempID")}`, requestOptions);
+        localStorage.removeItem("tempID")
+
+        // Handle successful login
+        if (response.status === 202) {
+            // Clear error message
+            document.getElementById("error-message").innerHTML = '';
+            // Show success message
+            Swal.fire({
+                title: 'Success',
+                text: `${name}'s account has been modified`,
+                icon: 'success',
+                confirmButtonColor: '#5156be',
+            }).then(() => window.location.href = "../accounts-management.html");
+        } else if (response.status === 404) {               // SHOULD NOT OCCUR
+            const outputHTML = 'Error: Account ID does not exist. Please try again.';
+            document.getElementById("error-message").innerHTML = outputHTML;
+        } else if (response.status === 409) {               // Handle incorrect username or password
+            const outputHTML = 'Error: Username already exists. Please try again.';
+            document.getElementById("error-message").innerHTML = outputHTML;
+        } else if (response.status === 500) {               // Handle non-existent account
+            const outputHTML = 'Error: Unable to create modify account information'
+            document.getElementById("error-message").innerHTML = outputHTML;
+        } else {                                            // Handle unexpected error
+            const outputHTML = 'An unexpected error occured, please try again later.'
+            document.getElementById("error-message").innerHTML = outputHTML;
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
