d66fd289d2c473168955e590865858c6eed01127
Jia Chen <github@jiachen.app>
2024-02-15 11:19:21 +0800

    feat: create paths@deploy.js

---
+++ js/paths@deploy.js
@@ -0,0 +1,3 @@
+const AUTH_SERVICE = "http://tsao-backend.hotchocolate.app:8000/api/auth"
+const RECORDS_SERVICE = "http://tsao-backend.hotchocolate.app:8001/api/records"
+const ACCOUNTS_SERVICE = "http://tsao-backend.hotchocolate.app:8002/api/accounts"
